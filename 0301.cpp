e2c2864fa104e1acf10008b04734a09d2f664745
Zzhangyuge <145735818+Zzhangyuge@users.noreply.github.com>
2023-10-26 18:19:17 +0800

    Add files via upload

---
+++ 0301.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "请输入两个数";
+	cin>> a >>b;
+	if (a < b) {
+
+	
+		cout << "a的值是" << a <<"\n" << "b的值是" << b<<"\n" << "a<b";
+
+	}
+	else if (a > b) {
+
+		cout <<"a的值是"<<a<<"\n" << "b的值是" << b<<"\n" << "a>b";
+	}
+	else {
+	
+		cout << "a的值是" << a <<"\n" << "b的值是" << b<<"\n" << "a=b";
+
+	}
+	return 0;
+}
